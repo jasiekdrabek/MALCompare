1424f2e3c185f9cc3195447425eda12f825ef97b
Jan Drabek <jasiekdrabek@gmail.com>
2026-07-05 00:01:03 +0200

    urls

--- mal_project/mal_project/urls.py
+++ mal_project/mal_project/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
 
 urlpatterns = [
 path('admin/', admin.site.urls),
@@ -24,7 +23,6 @@ path('', include('anime_compare.urls')), # utworzymy anime_compare/urls.py
 ]
 
 # fmt: off
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# fmt: on
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns += staticfiles_urlpatterns()
